d442d69426cec2209c14ccade26a7b5799ea1be4
Tyler Matteson <tyler@agritheory.com>
2022-08-10 21:01:28 -0400

    feat: add tpying

--- atnacha/nacha.py
+++ atnacha/nacha.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass, field
 import datetime
 import math
+from typing import Optional
 
 
 @dataclass
@@ -19,11 +20,11 @@ class NACHAFile:
     batches: list = field(default_factory=list)
 
     @property
-    def record_size(self):
+    def record_size(self) -> int:
         return 94
 
     @property
-    def block_count(self):
+    def block_count(self) -> int:
         # The total number of records included in the ACH file, rounded up to the nearest 10, then divided by 10. (08-13) 6 numeric.
         total_entries = (
             sum([len(batch.entries) for batch in self.batches])
@@ -33,45 +34,45 @@ class NACHAFile:
         return math.ceil(total_entries / 10)
 
     @property
-    def entry_addenda_acount(self):
+    def entry_addenda_account(self) -> int:
         return sum([len(batch.entries) for batch in self.batches])
 
     @property
-    def entry_hash(self):
+    def entry_hash(self) -> str:
         # The sum of all Entry Hash fields in the ACH file. If the total contains more digits than the field size allows, the number is automatically truncated. (22-31) 10 numeric.
         entry_hash = sum([int(batch.entry_hash) for batch in self.batches])
-        entry_hash = (
+        _entry_hash = (
             str(entry_hash)[len(str(entry_hash)) - 10 :]
             if len(str(entry_hash)) > 10
             else str(entry_hash)
         )
-        return f"{entry_hash:0>10}"
+        return f"{_entry_hash:0>10}"
 
     @property
-    def total_credit(self):
+    def total_credit(self) -> int:
         return sum([batch.total_credit for batch in self.batches])
 
     @property
-    def total_debit(self):
+    def total_debit(self) -> int:
         return sum([batch.total_debit for batch in self.batches])
 
     @property
-    def header(self):
+    def header(self) -> str:
         # position 1 2-3                   4-13                             14-23                       24-29                                     30-33                                   34                     35-37                 38-39                   40                 41-63                            64-86                       87-94
         return f"""1{self.priority_code:02}{self.immediate_destination: >10}{self.immediate_origin: >10}{self.file_creation_date.strftime('%y%m%d')}{self.file_creation_time.strftime('%H%M')}{self.file_id_modifier}{self.record_size:003}{self.blocking_factor:02}{self.format_code}{self.immediate_destination_name: <23}{self.immediate_origin_name: <23}{self.reference_code: >8}"""
 
     @property
-    def control(self):
+    def control(self) -> str:
         # position 1 2-7                   8-13                14-23                       24-29                                     30-33                                   34                     35-37                 38-39                   40                 41-63                            64-86                       87-94
-        return f"""9{len(self.batches):06}{self.block_count:06}{self.entry_addenda_acount:08}{self.entry_hash}{self.total_debit:0>12}{self.total_credit:0>12}{'': >39}"""
+        return f"""9{len(self.batches):06}{self.block_count:06}{self.entry_addenda_account:08}{self.entry_hash}{self.total_debit:0>12}{self.total_credit:0>12}{'': >39}"""
 
-    def batch_number(self, batch):
+    def batch_number(self, batch: 'ACHBatch') -> str:
         return f"{self.batches.index(batch) + 1:>07}"
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self()
 
-    def __call__(self):
+    def __call__(self) -> str:
         output = f"{self.header}\n"
         output += "\n".join(
             f"{batch(batch_number=self.batch_number(batch))}" for batch in self.batches
@@ -108,7 +109,7 @@ class ACHBatch:
     entries: list = field(default_factory=list)
 
     @property
-    def header(self):
+    def header(self) -> str:
         company_descriptive_date = (
             self.company_descriptive_date if self.company_descriptive_date else "000000"
         )
@@ -117,11 +118,11 @@ class ACHBatch:
         return f"""5{self.service_class_code}{self.company_name[:16]: <16}{self.company_discretionary_data: <20}{self.company_id: <10}{self.standard_class_code: <3}{self.company_entry_description: <10}{company_descriptive_date}{self.effective_entry_date.strftime('%y%m%d')}{settlement_date}{self.originator_status_code}{self.originating_dfi_id[:8]}"""
 
     @property
-    def control(self):
+    def control(self) -> str:
         return f"""8{self.service_class_code}{len(self.entries):06}{self.entry_hash}{self.total_debit:0>12}{self.total_credit:0>12}{self.company_id}{"": >19}{"": >6}{self.originating_dfi_id[:8]}"""
 
     @property
-    def total_credit(self):
+    def total_credit(self) -> int:
         return sum(
             [
                 getattr(entry, "amount", 0)
@@ -131,7 +132,7 @@ class ACHBatch:
         )
 
     @property
-    def total_debit(self):
+    def total_debit(self) -> int:
         return sum(
             [
                 getattr(entry, "amount", 0)
@@ -141,28 +142,28 @@ class ACHBatch:
         )
 
     @property
-    def entry_hash(self):
+    def entry_hash(self) -> str:
         entry_hash = sum(
             [
-                int(getattr(entry, "receiving_dfi_identification", 0)[:8])
+                int(str(getattr(entry, "receiving_dfi_identification", 0))[:8])
                 for entry in self.entries
             ]
         )
-        entry_hash = (
+        _entry_hash = (
             str(entry_hash)[len(str(entry_hash)) - 10 :]
             if len(str(entry_hash)) > 10
             else str(entry_hash)
         )
-        return f"{entry_hash:0>10}"
+        return f"{_entry_hash:0>10}"
 
-    def trace_number(self, entry):
+    def trace_number(self, entry) -> str:
         # A 15-digit number in which positions 1-8 are the first eight digits of the originator's routing number, and positions 9-15 are numbers assigned in ascending order to each transaction within the Company / Batch Header Record. (80-94) 15 numeric.
-        return self.originating_dfi_id[0:8] + f"{self.entries.index(entry) + 1:>07}"
+        return f"{self.originating_dfi_id[0:8]}{self.entries.index(entry) + 1:>07}"
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self()
 
-    def __call__(self, batch_number=1):
+    def __call__(self, batch_number: int=1) -> str:
         output = f"{self.header}{batch_number:0>7}\n"
         output += (
             "\n".join(f"{entry()}{self.trace_number(entry)}" for entry in self.entries)
@@ -183,29 +184,30 @@ class ACHEntry:
     individual_name: str = ""
     discretionary_data: str = ""
     addenda_record_indicator: str = ""
-    batch: ACHBatch = None
+    batch: Optional['ACHBatch'] = None
 
-    def __str__(self):
+    def __str__(self) -> str:
         # position 1 2-3                      4-11                                    12                13-29                         30-39            40-54                            55-76                           77-78                        79
         return f"""6{self.transaction_code:02}{self.receiving_dfi_identification[0:8]}{self.check_digit}{self.dfi_account_number: <17}{self.amount:010}{self.individual_id_number: <15}{self.individual_name[0:22]: <22}{self.discretionary_data: <2}{self.addenda_record_indicator}"""
 
-    def __call__(self):
+    def __call__(self) -> str:
         return self.__str__()
 
 
-{
-    22: "Checking Credit Live",
-    23: "Checking Credit Prenote",
-    24: "Checking Credit Child Support prenote",
-    27: "Checking Debit Live",
-    28: "Checking Debit Prenote",
-    29: "Checking Debit Child Support prenote",
-    32: "Savings Credit Live",
-    33: "Savings Credit Prenote",
-    34: "Savings Credit Child Support prenote",
-    37: "Savings Debit Live",
-    38: "Savings Debit Prenote",
-    39: "Savings Debit Child Support prenote",
-    52: "Loan Credit Live",
-    53: "Loan Credit Prenote",
-}
+# this will probably become an enum nad exported as part of the package
+# {
+#     22: "Checking Credit Live",
+#     23: "Checking Credit Prenote",
+#     24: "Checking Credit Child Support prenote",
+#     27: "Checking Debit Live",
+#     28: "Checking Debit Prenote",
+#     29: "Checking Debit Child Support prenote",
+#     32: "Savings Credit Live",
+#     33: "Savings Credit Prenote",
+#     34: "Savings Credit Child Support prenote",
+#     37: "Savings Debit Live",
+#     38: "Savings Debit Prenote",
+#     39: "Savings Debit Child Support prenote",
+#     52: "Loan Credit Live",
+#     53: "Loan Credit Prenote",
+# }
